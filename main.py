b17458c85cd6455973f8dc5170352d2be4bf2e3c
eesuck1 <eesucksc@gmail.com>
2023-03-30 23:09:08 +0300

    Видалив файл

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import dataset
 def main():
     scr = scrapper.Scrapper()
     scr.scrap()
-    #
-    # dictionary = scr.get_data()
-    #
-    # data = dataset.Dataset(dictionary)
-    #
-    # data.create_excel(data.create_path("Data", "sheet"))
+
+    dictionary = scr.get_data()
+
+    data = dataset.Dataset(dictionary)
+
+    data.create_csv(data.create_path("Data", "comma"))
 
 
 if __name__ == '__main__':
