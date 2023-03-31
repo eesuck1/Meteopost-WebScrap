2b60e78c25a1dce10a7e8f50b7b34d222aaa17ca
eesuck1 <eesucksc@gmail.com>
2023-03-31 23:37:12 +0300

    Нормалізація

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ URL = "https://meteopost.com/weather/climate/"
 YEAR_XPATH = '/html/body/table[1]/tbody/tr[3]/td[1]/form[1]/select[1]'
 SELECT_XPATH = '/html/body/table[1]/tbody/tr[3]/td[1]/form[1]/select[2]'
 BUTTON_XPATH = '/html/body/table[1]/tbody/tr[3]/td[1]/form[1]/input[2]'
-LVIV_FIELD_XPATH = '/html/body/table[1]/tbody/tr[3]/td[1]/form[1]/select[2]/option[55]'
 CATEGORIES = ["City", "Date", "Average Temperature",
               "Max Temperature", "Min Temperature", "Average Wind Speed",
               "Total Precipitation", "Max Snow Depth"]
+
--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,15 @@
-import time
+import os
 import re
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import Select
 import bs4
 
 import constants
+import dataset
 
 
 class Scrapper:
@@ -24,47 +26,62 @@ class Scrapper:
         self.__options__ = None
         self.__soup__ = None
         self.__dictionary__ = None
+        self.__frame__ = None
 
     @staticmethod
-    def __start_info__():
+    def __start_info__() -> None:
         print(f"[INFO] Work is started.")
 
     @staticmethod
-    def __done_info__():
+    def __done_info__() -> None:
         print(f"[INFO] Work is done.")
 
-    def scrap(self) -> None:
+    def __create_dictionary__(self) -> None:
+        self.__dictionary__ = {category: [] for category in constants.CATEGORIES}
+
+    def scrap(self, result_folder: str) -> None:
         self.__start_info__()
 
-        self.__dictionary__ = {category: [] for category in constants.CATEGORIES}
         self.__options__ = Options()
         self.__options__.headless = True
 
         self.__browser__ = webdriver.Edge(options=self.__options__)
         self.__browser__.get(constants.URL)
 
-        self.__full_parse__()
+        self.__full_parse__(result_folder)
 
         self.__done_info__()
 
-    def __full_parse__(self) -> None:
+    def __full_parse__(self, result_folder: str) -> None:
         self.years = Select(self.__browser__.find_element(By.XPATH, constants.YEAR_XPATH))
 
-        for i in range(len(self.years.options)):
+        for year_index in range(len(os.listdir(result_folder)), len(self.years.options)):
+            self.__create_dictionary__()
+
             self.years = Select(self.__browser__.find_element(By.XPATH, constants.YEAR_XPATH))
-            self.years.select_by_index(i)
+            self.years.select_by_index(year_index)
 
             self.__update__()
-            self.__parse__(self.__browser__.page_source, "Lviv")
+
+            self.__frame__ = dataset.Dataset(self.get_data())
+            self.__frame__.create_csv(self.__frame__.create_path(result_folder, f"{2010 + year_index}"))
 
     def __update__(self) -> None:
-        selected_element = self.__browser__.find_element(By.XPATH, constants.SELECT_XPATH)
-        selected_element.click()
+        self.cities = Select(self.__browser__.find_element(By.XPATH, constants.SELECT_XPATH))
 
-        selected_element.find_element(By.XPATH, constants.LVIV_FIELD_XPATH).click()
+        for city_index in range(len(self.cities.options)):
+            try:
+                self.cities = Select(self.__browser__.find_element(By.XPATH, constants.SELECT_XPATH))
+                city = self.cities.options[city_index].text
 
-        button = self.__browser__.find_element(By.XPATH, constants.BUTTON_XPATH)
-        button.click()
+                self.cities.select_by_index(city_index)
+
+                button = self.__browser__.find_element(By.XPATH, constants.BUTTON_XPATH)
+                button.click()
+
+                self.__parse__(self.__browser__.page_source, city)
+            except WebDriverException:
+                continue
 
     def __parse__(self, page_source: str, city: str) -> None:
         self.__soup__ = bs4.BeautifulSoup(page_source, "html.parser")
